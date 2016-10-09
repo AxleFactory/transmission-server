b241e768642d89047ef088bc12d15a761de6c1ad
Jacob LeGrone <git@jacoblegrone.com>
2016-10-08 22:04:56 -0400

    Add priority
    Closes #3

--- Transmission/models.py
+++ Transmission/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MaxValueValidator
 
 
 class ModelWithDates(models.Model):
@@ -24,13 +25,29 @@ class TextAction(Action):
 
 class Assignment(ModelWithDates):
     name = models.CharField(max_length=30)
-    active = models.BooleanField(default=False)
+    active = models.BooleanField(
+        default=False,
+        help_text="Only assignments marked 'Active' will be shown to users. See <a href='/all-assignments.json' target='_blank'>all-assignments.json</a> for an unfiltered view containing expired and inactive assignments.",
+        db_index=True
+    )
     description = models.CharField(max_length=200)
     instructions = models.CharField(max_length=200)
-    expiration = models.DateTimeField('expiration date')
-    require_call_first = models.BooleanField(default=False)
+    expiration = models.DateTimeField('expiration date', db_index=True)
+    priority = models.PositiveSmallIntegerField(
+        default=0,
+        help_text="Assignments will be sorted from highest to lowest priority before being shown to users.  Assignments with the same priority will be sorted by expiration date.",
+        validators=[MaxValueValidator(100)],
+        db_index=True
+    )
+    require_call_first = models.BooleanField(
+        default=False,
+        help_text="If selected, the user will have to complete a call action for the current assignment before text actions become available."
+    )
     call_actions = models.ManyToManyField(CallAction, blank=True)
     text_actions = models.ManyToManyField(TextAction, blank=True)
+
+    class Meta:
+        ordering = ['-priority', 'expiration']
 
     @property
     def expired(self):
