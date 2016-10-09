589219f38538d9b455b91366beaa7da856351891
Jacob LeGrone <git@jacoblegrone.com>
2016-10-09 01:39:32 -0400

    Customize admin
    Closes #2

--- Transmission/admin.py
+++ Transmission/admin.py
@@ -1,9 +1,9 @@
-from django.contrib.admin import AdminSite
+from django.contrib import admin
 from django.utils.translation import ugettext_lazy
 
 from .models import Assignment, CallAction, TextAction
 
-class TransmissionAdmin(AdminSite):
+class TransmissionAdmin(admin.AdminSite):
     # Text to put at the end of each page's <title>.
     site_title = ugettext_lazy('Transmission Admin')
 
@@ -15,4 +15,33 @@ class TransmissionAdmin(AdminSite):
 
 admin_site = TransmissionAdmin(name='assignments')
 
-admin_site.register([Assignment, CallAction, TextAction])
+
+@admin.register(CallAction, site=admin_site)
+class CallActionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'call_script']
+    search_fields = ['name', 'call_script']
+
+
+@admin.register(TextAction, site=admin_site)
+class TextActionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'message_content']
+    search_fields = ['name', 'message_content']
+
+
+@admin.register(Assignment, site=admin_site)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'expiration', 'expired', 'priority', 'active']
+    list_filter = ['active', 'expiration', 'call_actions', 'text_actions']
+    search_fields = ['name', 'description', 'instructions']
+    fieldsets = [
+        (None, {
+            'fields': ('name', 'description', 'instructions')
+        }),
+        ('Actions', {
+            'fields': (('call_actions', 'text_actions'), 'require_call_first'),
+        }),
+        ('Availability & Sorting', {
+            'classes': ('collapse',),
+            'fields': ('active', 'expiration', 'priority'),
+        }),
+    ]
