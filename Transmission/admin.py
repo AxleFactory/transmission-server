60b72396b8e794fc580825265d9f6add212db8b4
Jacob LeGrone <git@jacoblegrone.com>
2016-10-09 02:16:27 -0400

    Remove site url

--- Transmission/admin.py
+++ Transmission/admin.py
@@ -3,15 +3,15 @@ from django.utils.translation import ugettext_lazy
 
 from .models import Assignment, CallAction, TextAction
 
+
 class TransmissionAdmin(admin.AdminSite):
     # Text to put at the end of each page's <title>.
     site_title = ugettext_lazy('Transmission Admin')
-
     # Text to put in each page's <h1> (and above login form).
     site_header = ugettext_lazy('Transmission Administration')
-
     # Text to put at the top of the admin index page.
     index_title = ugettext_lazy('Edit Assignments')
+    site_url = None
 
 admin_site = TransmissionAdmin(name='assignments')
 
@@ -35,13 +35,13 @@ class AssignmentAdmin(admin.ModelAdmin):
     search_fields = ['name', 'description', 'instructions']
     fieldsets = [
         (None, {
-            'fields': ('name', 'description', 'instructions')
+            'fields': ['name', 'description', 'instructions']
         }),
         ('Actions', {
-            'fields': (('call_actions', 'text_actions'), 'require_call_first'),
+            'fields': [('call_actions', 'text_actions'), 'require_call_first'],
         }),
         ('Availability & Sorting', {
-            'classes': ('collapse',),
-            'fields': ('active', 'expiration', 'priority'),
+            'classes': ['collapse'],
+            'fields': ['active', 'expiration', 'priority'],
         }),
     ]
