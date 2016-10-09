03e781ac40114d98cf20a0af5cb31a5670f41b85
Jacob LeGrone <git@jacoblegrone.com>
2016-10-09 18:56:32 -0400

    Format JSON responses

--- Transmission/views.py
+++ Transmission/views.py
@@ -4,14 +4,28 @@ from django.views.decorators.cache import cache_page
 from .models import Assignment
 
 
-@cache_page(60 * 15) # cache view for 15 minutes
+@cache_page(60 * 15)  # cache view for 15 minutes
 def assignments(request):
-    all_assignments = [a.get_json() for a in Assignment.objects.all().filter(active=True) if not a.expired]
-    return JsonResponse(all_assignments, safe=False)
+    active_contact_assignments = [a.get_json() for a in Assignment.objects.all().filter(active=True) if not a.expired]
+    return JsonResponse(
+        active_contact_assignments,
+        safe=False,
+        json_dumps_params={
+            'separators': (',', ':')
+        }
+    )
+
 
 def all_assignments(request):
-    all_assignments = [a.get_json() for a in Assignment.objects.all()]
-    return JsonResponse(all_assignments, safe=False)
+    all_contact_assignments = [a.get_json() for a in Assignment.objects.all()]
+    return JsonResponse(
+        all_contact_assignments,
+        safe=False,
+        json_dumps_params={
+            'indent': 4,
+            'sort_keys': True,
+        }
+    )
 
 
 def assignments_redirect(request):
