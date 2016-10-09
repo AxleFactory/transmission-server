2f3874ebcd29e525dcb656e4c656c445f7954fc7
Jacob LeGrone <git@jacoblegrone.com>
2016-10-08 23:00:04 -0400

    Add caching for assignments
    Closes #1

--- Transmission/views.py
+++ Transmission/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import redirect
 from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
 from .models import Assignment
 
 
+@cache_page(60 * 15) # cache view for 15 minutes
 def assignments(request):
     all_assignments = [a.get_json() for a in Assignment.objects.all().filter(active=True) if not a.expired]
     return JsonResponse(all_assignments, safe=False)
